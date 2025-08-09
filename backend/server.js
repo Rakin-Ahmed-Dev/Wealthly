1ce7e823fa4a1ae1720a6d0b30df2b728944f03b
Rakin Ahmed <Rakin.ahmed.dev@gmail.com>
2025-08-09 13:49:04 -0500

    Add backend code to repo, Axios instances, and API credentials

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+// Load enviornment variables from the .env file
+require("dotenv").config();
+
+// Import necessary modules
+const express = require("express"); // Framework to create the server
+const cors = require("cors"); // Enables requests from the frontend to backend
+const path = require("path"); // Helps work with file and folder paths
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const incomeRoutes = require("./routes/incomeRoutes");
+const expenseRoutes = require("./routes/expenseRoutes");
+const dashboardRoutes = require("./routes/dashboardRoutes");
+
+const app = express();
+
+// Set up CORS middleware to allow frontend requests
+// Middleware to handle CORS
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL || "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
+
+app.use(express.json());
+
+connectDB();
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/income", incomeRoutes);
+app.use("/api/v1/expense", expenseRoutes);
+app.use("/api/v1/dashboard", dashboardRoutes);
+
+
+// Serve uploads folder 
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
